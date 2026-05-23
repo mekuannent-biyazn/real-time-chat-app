c1bd1815e19abc34f89d853898947baa9b842472
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-05-23 20:20:14 +0300

    update backend url api

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "https://real-time-chat-app-ia9k.onrender.com/api"||"http://localhost:5001/api"
+      : `${import.meta.env.VITE_BACKEND_URL}/api`,
   withCredentials: true,
 });
 
